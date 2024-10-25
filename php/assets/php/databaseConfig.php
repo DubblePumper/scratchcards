7f7808d303e3006972354a0fc24ad33210be2365
DubblePumper <sander.truyts@gmail.com>
2024-10-25 18:01:59 +0200

    Refactor databaseConfig.php and .gitignore

--- php/assets/php/databaseConfig.php
+++ php/assets/php/databaseConfig.php
@@ -1,8 +1,8 @@
 <?php
 //config settings such as DB connection parameters, site URLs, API keys, etc
-define("DB_HOST", "HERE THE IP"); // Use the service name defined in docker-compose.yaml
-define("DB_PORT", "HERE THE PORT");
-define("DB_DATABASE", "HERE DATABASE NAME");
-define("DB_USERNAME", "HERE DATABASE USERNAME");
-define("DB_PASSWORD", "HERE DATABASE PASSWORD");
+define("DB_HOST", "217.119.143.38"); // Use the service name defined in docker-compose.yaml
+define("DB_PORT", "3306");
+define("DB_DATABASE", "movkcltg_scratch");
+define("DB_USERNAME", "movkcltg_scratch");
+define("DB_PASSWORD", "9SYkCzngbMzS2GNXzTqr");
 ?>
