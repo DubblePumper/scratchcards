fb07b9aea6b51ed7efe92f1a711fa1f53694e1ba
DubblePumper <67190691+DubblePumper@users.noreply.github.com>
2024-10-25 15:51:53 +0200

    Update databaseConfig.php

--- php/assets/php/databaseConfig.php
+++ php/assets/php/databaseConfig.php
@@ -1,8 +1,5 @@
 <?php
 //config settings such as DB connection parameters, site URLs, API keys, etc
-define("DB_HOST", "217.119.143.38"); // Use the service name defined in docker-compose.yaml
-define("DB_PORT", "3306");
-define("DB_DATABASE", "movkcltg_scratch");
-define("DB_USERNAME", "movkcltg_scratch");
-define("DB_PASSWORD", "9SYkCzngbMzS2GNXzTqr");
+
+
 ?>
