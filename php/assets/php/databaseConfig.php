e56b70bf3fc5ce63e3026384938dae1c03eb4c70
DubblePumper <67190691+DubblePumper@users.noreply.github.com>
2024-10-25 18:00:59 +0200

    Update databaseConfig.php

--- php/assets/php/databaseConfig.php
+++ php/assets/php/databaseConfig.php
@@ -1,5 +1,8 @@
 <?php
 //config settings such as DB connection parameters, site URLs, API keys, etc
-
-
+define("DB_HOST", "HERE THE IP"); // Use the service name defined in docker-compose.yaml
+define("DB_PORT", "HERE THE PORT");
+define("DB_DATABASE", "HERE DATABASE NAME");
+define("DB_USERNAME", "HERE DATABASE USERNAME");
+define("DB_PASSWORD", "HERE DATABASE PASSWORD");
 ?>
